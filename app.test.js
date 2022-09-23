06557c481fcba8fe1e30e9ba484c3ac425f980e7
hasindusithmin <hasindusithmin64@gmail.com>
2022-09-23 23:10:40 +0530

    PASS GET /student/:id ✓ should return a student

--- app.test.js
+++ app.test.js
@@ -3,7 +3,7 @@ const supertest = require('supertest');
 const assert = require('assert');
 const app = require('./app');
 const pool = require('./db');
-
+const {faker} = require('@faker-js/faker')
 
 // describe('GET /status', () => {
 //     it('should return OK', async () => {
@@ -13,19 +13,36 @@ const pool = require('./db');
 //     });
 // });
 
-describe('GET /student', () => {
-    it('should return all students', async () => {
-        const response = await supertest(app).get('/student');
-        const db = await pool.query("SELECT * FROM student;");
+// describe('GET /student', () => {
+//     it('should return all students', async () => {
+//         const response = await supertest(app).get('/student');
+//         const db = await pool.query("SELECT * FROM student;");
+//         assert.equal(response.status, 200);
+//         assert.equal(response.body.length, db.rows.length);
+//         for (let i = 0; i < response.body.length; i++) {
+//             assert.equal(response.body[i].id, db.rows[i].id);
+//             assert.equal(response.body[i].name, db.rows[i].name);
+//             assert.equal(response.body[i].email, db.rows[i].email);
+//             assert.equal(response.body[i].city, db.rows[i].city);
+//             assert.equal(response.body[i].phone_number, db.rows[i].phone_number);
+//             assert.equal(response.body[i].text, db.rows[i].text);
+//         }
+//     })
+// });
+
+describe('GET /student/:id', () => {
+    it('should return a student', async () => {
+        const id = '8ebd84b8-90b2-4aa2-94ba-5645938e61b8'
+        const db = await pool.query(`SELECT * FROM student WHERE id = '${id}';`);
+        const response = await supertest(app).get(`/student/${id}`);
         assert.equal(response.status, 200);
-        assert.equal(response.body.length, db.rows.length);
-        for (let i = 0; i < response.body.length; i++) {
-            assert.equal(response.body[i].id, db.rows[i].id);
-            assert.equal(response.body[i].name, db.rows[i].name);
-            assert.equal(response.body[i].email, db.rows[i].email);
-            assert.equal(response.body[i].phone_number, db.rows[i].phone_number);
-            assert.equal(response.body[i].text, db.rows[i].text);
-        }
-    })
+        assert.equal(response.body.id, db.rows[0].id);
+        assert.equal(response.body.name, db.rows[0].name);
+        assert.equal(response.body.email, db.rows[0].email);
+        assert.equal(response.body.city, db.rows[0].city);
+        assert.equal(response.body.phone_number, db.rows[0].phone_number);
+        assert.equal(response.body.text, db.rows[0].text);
+    });
 });
+
 
--- routes/student.js
+++ routes/student.js
@@ -13,5 +13,24 @@ studentRoute.get('/', async(req, res) => {
     }
 });
 
+studentRoute.get('/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const result = await pool.query("SELECT * FROM student WHERE id = $1;", [id]);
+        res.status(200).json(result.rows[0]);
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+})
+
+studentRoute.post('/', async(req, res) => {
+    try {
+        const {name,email,city,phone,text} = req.body;
+        const result = await pool.query("INSERT INTO student (name,email,city,phone,text) VALUES ($1,$2,$3,$4,$5);", [name,email,city,phone,text]);
+        res.status(201).json(result);
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+});
 
 module.exports = studentRoute;
